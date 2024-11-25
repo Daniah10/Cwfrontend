44568ef7541120df8c91b19786d3acd8f3eaaf94
Daniah10 <156360838+Daniah10@users.noreply.github.com>
2024-11-25 14:30:45 +0000

    Finished sort functionality

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
 import{ref} from "vue"
 import LessonCard from "./components/LessonCard.vue";
+import SortSearchBar from "./components/SortSearchBar.vue";
 const lessons= ref([])
+const sortProperty= ref("")
+const sortOrder= ref("")
 const getLessons= async () => {
   const response= await fetch ("https://applicationbackend-58pp.onrender.com/lessons")
   const data= await response.json()
@@ -9,9 +12,28 @@ const getLessons= async () => {
 }
 getLessons()
 
+const changeSort= (property, order)=> {
+  if(! property || ! order) {
+    return
+  } 
+  sortProperty.value = property 
+  sortOrder.value = order
+  
+  lessons.value.sort((a, b) => {
+    if (property === 'space' || property === 'price') {
+      return order === 'asc' ? Number(a[property]) - Number(b[property]) : Number(b[property]) - Number(a[property])
+    } else {
+      return order === 'asc' ? a[property].localeCompare(b[property]) : b[property].localeCompare(a[property])
+    }
+  })
+
+  lessons.value = [...lessons.value]
+}
+
 </script>
 
 <template>
+<SortSearchBar :changeSort="changeSort"/>  
 <LessonCard v-for="lesson in lessons" :key="lesson.id" :lesson="lesson"/>
   
 
---
+++ vue-project/src/components/SortSearchBar.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="sort-search-bar">
+
+    <div class="sort-section">
+      <select id="sort-field" v-model="sortCategory">
+        <option disabled value="">Field</option>
+        <option value="topic">Subject</option>
+        <option value="location">Location</option>
+        <option value="price">Price</option>
+        <option value="space">Spaces</option>
+      </select>
+    </div>
+
+    <div class="sort-section">
+      <select id="sort-order" v-model="sortOrder">
+        <option disabled value="">Order</option>
+        <option value="asc">Ascending</option>
+        <option value="desc">Descending</option>
+      </select>
+    </div>
+
+    <button v-on:click="changeSort(sortCategory, sortOrder)" class="apply-button">Apply</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  changeSort: {
+    type: Function,
+    required: true,
+  }
+})
+
+const sortCategory = ref('')
+const sortOrder = ref('')
+
+</script>
+
+<style scoped>
+.sort-search-bar {
+  padding: 20px;
+  background-color: #333;
+  width: 100%;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  color: #ffffff;
+  margin-top: 70px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  border-radius: 0 0 15px 15px;
+}
+
+.sort-section {
+  margin-right: 20px;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+  font-weight: bold;
+}
+
+input,
+select {
+  width: 100%;
+  padding: 8px;
+  font-size: 1rem;
+  border-radius: 5px;
+}
+
+.apply-button {
+  padding: 10px 20px;
+  background-color: #fff;
+  color: #000000;
+  border: none;
+  cursor: pointer;
+  font-size: 1rem;
+  border-radius: 5px;
+}
+
+.apply-button:hover {
+  background-color: #555;
+}
+
+@media (max-width: 1110px) {
+  .sort-search-bar {
+    flex-direction: column;
+    gap: 10px;
+    align-items: center;
+  }
+  .sort-search-bar > * {
+    width: 100%;
+    align-items: center;
+    margin-right: 0;
+  }
+}
+</style>
