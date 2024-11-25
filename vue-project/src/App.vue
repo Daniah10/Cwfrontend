641456184b2254508c739ac38826e6668e96d359
Daniah10 <156360838+Daniah10@users.noreply.github.com>
2024-11-25 13:57:54 +0000

    initialized the app and displayed the lessons

---
+++ vue-project/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import{ref} from "vue"
+import LessonCard from "./components/LessonCard.vue";
+const lessons= ref([])
+const getLessons= async () => {
+  const response= await fetch ("https://applicationbackend-58pp.onrender.com/lessons")
+  const data= await response.json()
+  lessons.value= data
+}
+getLessons()
+
+</script>
+
+<template>
+<LessonCard v-for="lesson in lessons" :key="lesson.id" :lesson="lesson"/>
+  
+
+  
+</template>
+
+<style scoped>
+
+</style>
